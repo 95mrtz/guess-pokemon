d80e2dd76b219d4bb47b3c85b150f65c03164f1c
manuelrtz <manuelrtzdev@gmail.com>
2022-08-07 16:57:50 -0300

    Primera entrega finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,66 @@ import api from "./api";
 
 const App = () => {
   const imagenPokemon = useRef(null);
-  const [pokemon, setPokemon] = useState({id: 0, name: "", image: ""});
+  const [randomPokemon, setRandomPokemon] = useState({id: 0, name: "", image: ""});
+  const [pokemon, setPokemon] = useState("");
   const [guess, setGuess] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    api.random().then(setPokemon);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  useEffect(() => {
+    api.random().then(setRandomPokemon);
     setGuess(false);
   }, []);
 
-  // eslint-disable-next-line no-console
-  console.log(pokemon);
+  const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setPokemon(event.target.value);
+  };
 
-  const handleGuess = () => {
+  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     setGuess(true);
   };
 
   return (
-    <main>
-      <h1>Quien es este Pokemon?</h1>
-      <img
-        ref={imagenPokemon}
-        alt={pokemon.name}
-        className={guess ? "filterOff" : "filterOn"}
-        height="400px"
-        src={pokemon.image}
-        width="500px"
-      />
-      <button onClick={handleGuess}> adivinar </button>
-    </main>
+    <div className="app">
+      <header>
+        <p> POKEMON </p>
+      </header>
+      <main>
+        <h1>Quien es este Pokemon?</h1>
+        <div className="img-section">
+          {loading ? (
+            <h1> cargando...</h1>
+          ) : (
+            <img
+              ref={imagenPokemon}
+              alt={randomPokemon.name}
+              className={guess ? "filterOff" : "filterOn"}
+              height="300px"
+              src={randomPokemon.image}
+              width="400px"
+            />
+          )}
+        </div>
+        {guess ? <h2>{randomPokemon.name}</h2> : <h2> ??? </h2>}
+        <form className="form-guess" onSubmit={handleSubmit}>
+          <input
+            className="input-guess"
+            placeholder={"ingrese el nombre"}
+            type={"text"}
+            value={pokemon}
+            onChange={handleChange}
+            required
+          />
+          <input className="btn-guess" type="submit" value="adivinar" />
+        </form>
+      </main>
+    </div>
   );
 };
 
