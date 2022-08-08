270a11cdfa28be8b3d6150e6fd7a06a6096f9c43
manuelrtz <manuelrtzdev@gmail.com>
2022-08-08 13:13:22 -0300

    tercera entrega finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,22 @@ const App = () => {
   const [pokemon, setPokemon] = useState("");
   const [guess, setGuess] = useState(false);
   const [loading, setLoading] = useState(true);
-  //const [isCorrect, setIsCorrect] = useState(false);
+  const [intentosCount, setIntentosCount] = useState(localStorage.getItem("intentosCount") || 0);
   const [laRespuesta, setLaRespuesta] = useState("");
+  const [sucessCount, setSucessCount] = useState(localStorage.getItem("sucessCount") || 0);
+  const [errorCount, setErrorCount] = useState(localStorage.getItem("errorCount") || 0);
+
+  useEffect(() => {
+    return localStorage.setItem("sucessCount", sucessCount.toString());
+  }, [sucessCount]);
+
+  useEffect(() => {
+    localStorage.setItem("errorCount", errorCount.toString());
+  }, [errorCount]);
+
+  useEffect(() => {
+    localStorage.setItem("intentosCount", intentosCount.toString());
+  }, [intentosCount]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,6 +49,18 @@ const App = () => {
     }, time);
   };
 
+  const startGame = () => {
+    setTimeout(() => {
+      setLaRespuesta("");
+      setGuess(false);
+      setPokemon("");
+      setSucessCount(Number(0));
+      setErrorCount(Number(0));
+      setIntentosCount(Number(0));
+      api.random().then(setRandomPokemon);
+    }, 1000);
+  };
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPokemon(event.target.value);
   };
@@ -49,28 +75,42 @@ const App = () => {
     if (pokemonOnlyLetters === randomPokemon.name) {
       setGuess(true);
       setLaRespuesta("Correcto! es");
-      nextPokemon(7000);
+      setSucessCount(Number(sucessCount) + 1);
+      setIntentosCount(Number(intentosCount) + 1);
+      nextPokemon(4000);
     } else {
       setLaRespuesta("Oh no! incorrecto :( ");
-      nextPokemon(4000);
+      setErrorCount(Number(errorCount) + 1);
+      setIntentosCount(Number(intentosCount) + 1);
+      nextPokemon(2000);
     }
   };
 
   return (
     <div className="app">
       <header>
-        <a>
-          <p>github</p>
+        <a
+          href="https://github.com/hctmanuelortiz/guess-pokemon"
+          rel="noreferrer"
+          target={"_blank"}
+        >
+          <i className="nes-icon github is-medium" />
         </a>
-        <a>
+        <a href="https://github.com/goncy/interview-challenges" rel="noreferrer" target={"_blank"}>
           <p> Challenge by Goncy </p>
         </a>
-        <a>
-          <p>twitter</p>
+        <a href="https://twitter.com/hctmanuelortiz" rel="noreferrer" target={"_blank"}>
+          <i className="nes-icon twitter is-medium" />
         </a>
       </header>
       <main>
-        <h1>Quien es este Pokemon?</h1>
+        <h1 className="title-section">Quien es este Pokemon?</h1>
+
+        <div className="nes-container is-rounded is-dark count-section">
+          <h3 className="nes-text is-disabled intentos"> Intentos: {intentosCount} </h3>
+          <h3 className="nes-text is-success aciertos"> aciertos: {sucessCount} </h3>
+          <h3 className="nes-text is-error errores"> errores: {errorCount} </h3>
+        </div>
         <section className="pokemon-section">
           {loading ? (
             <h1> cargando...</h1>
@@ -79,9 +119,7 @@ const App = () => {
               ref={imagenPokemon}
               alt={randomPokemon.name}
               className={guess ? "filterOff" : "filterOn"}
-              height="300px"
               src={randomPokemon.image}
-              width="400px"
             />
           )}
           <h2> {laRespuesta} </h2>
@@ -95,15 +133,21 @@ const App = () => {
           <form className="form-guess" onSubmit={handleSubmit}>
             <input
               required
-              className="input-guess"
+              className="nes-input nes-pointer input-guess"
               placeholder={"ingrese el nombre"}
               type={"text"}
               value={pokemon}
               onChange={handleChange}
             />
-            <input className="btn-guess" type="submit" value="adivinar" />
+            <input
+              className="nes-btn is-primary nes-pointer btn-guess"
+              type="submit"
+              value="adivinar"
+            />
           </form>
-          <button onClick={() => nextPokemon(1000)}>Volver a jugar</button>
+          <button className="nes-btn is-warning" onClick={startGame}>
+            Volver a empezar
+          </button>
         </section>
       </main>
     </div>
